f7a220a456a325295b0e73cbc40c24251d637cc4
xyn3037 <xyn3037@corp.netease.com>
2018-03-06 16:35:39 +0800

    a

--- linux/example3/client3.cpp
+++ linux/example3/client3.cpp
@@ -3,91 +3,141 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/select.h>
+#include <sys/epoll.h>
 #include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <errno.h>
 #include <arpa/inet.h>
-#define MAXLINE 1024
-#define IPADDRESS "127.0.0.1"
-#define SERV_PORT 8787
 
-#define max(a,b) (a > b) ? a : b
+#define MAXSIZE     1024
+#define IPADDRESS   "127.0.0.1"
+#define SERV_PORT   8787
+#define FDSIZE        1024
+#define EPOLLEVENTS 20
 
-static void handle_recv_msg(int sockfd, char *buf) 
-{
-    printf("client recv msg is:%s\n", buf);
-    sleep(5);
-    write(sockfd, buf, strlen(buf) +1);
-}
-
-static void handle_connection(int sockfd)
-{
-    char sendline[MAXLINE],recvline[MAXLINE];
-    int maxfdp,stdineof;
-    fd_set readfds;
-    int n;
-    struct timeval tv;
-    int retval = 0;
-
-    while (1) {
-
-        FD_ZERO(&readfds);
-        FD_SET(sockfd,&readfds);
-        maxfdp = sockfd;
-
-        tv.tv_sec = 5;
-        tv.tv_usec = 0;
-
-        retval = select(maxfdp+1,&readfds,NULL,NULL,&tv);
-
-        if (retval == -1) {
-        return ;
-    }
-
-    if (retval == 0) {
-        printf("client timeout.\n");
-        continue;
-    }
-
-    if (FD_ISSET(sockfd, &readfds)) {
-    n = read(sockfd,recvline,MAXLINE);
-    if (n <= 0) {
-        fprintf(stderr,"client: server is closed.\n");
-        close(sockfd);
-        FD_CLR(sockfd,&readfds);
-        return;
-    }
-
-    handle_recv_msg(sockfd, recvline);
-    }
-  }
-}
+static void handle_connection(int sockfd);
+static void
+handle_events(int epollfd,struct epoll_event *events,int num,int sockfd,char *buf);
+static void do_read(int epollfd,int fd,int sockfd,char *buf);
+static void do_read(int epollfd,int fd,int sockfd,char *buf);
+static void do_write(int epollfd,int fd,int sockfd,char *buf);
+static void add_event(int epollfd,int fd,int state);
+static void delete_event(int epollfd,int fd,int state);
+static void modify_event(int epollfd,int fd,int state);
 
 int main(int argc,char *argv[])
 {
-    int sockfd;
-    struct sockaddr_in servaddr;
-
+    int                 sockfd;
+    struct sockaddr_in  servaddr;
     sockfd = socket(AF_INET,SOCK_STREAM,0);
-
     bzero(&servaddr,sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SERV_PORT);
     inet_pton(AF_INET,IPADDRESS,&servaddr.sin_addr);
-
-    int retval = 0;
-    retval = connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
-if (retval < 0) {
-    fprintf(stderr, "connect fail,error:%s\n", strerror(errno));
-    return -1;
+    connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+    //处理连接
+    handle_connection(sockfd);
+    close(sockfd);
+    return 0;
 }
 
-    printf("client send to server .\n");
-    write(sockfd, "hello server", 32);
 
-    handle_connection(sockfd);
+static void handle_connection(int sockfd)
+{
+    int epollfd;
+    struct epoll_event events[EPOLLEVENTS];
+    char buf[MAXSIZE];
+    int ret;
+    epollfd = epoll_create(FDSIZE);
+    add_event(epollfd,STDIN_FILENO,EPOLLIN);
+    for ( ; ; )
+    {
+        ret = epoll_wait(epollfd,events,EPOLLEVENTS,-1);
+        handle_events(epollfd,events,ret,sockfd,buf);
+    }
+    close(epollfd);
+}
 
-    return 0;
+static void
+handle_events(int epollfd,struct epoll_event *events,int num,int sockfd,char *buf)
+{
+    int fd;
+    int i;
+    for (i = 0;i < num;i++)
+    {
+        fd = events[i].data.fd;
+        if (events[i].events & EPOLLIN)
+            do_read(epollfd,fd,sockfd,buf);
+        else if (events[i].events & EPOLLOUT)
+            do_write(epollfd,fd,sockfd,buf);
+    }
+}
+
+static void do_read(int epollfd,int fd,int sockfd,char *buf)
+{
+    int nread;
+    nread = read(fd,buf,MAXSIZE);
+        if (nread == -1)
+    {
+        perror("read error:");
+        close(fd);
+    }
+    else if (nread == 0)
+    {
+        fprintf(stderr,"server close.\n");
+        close(fd);
+    }
+    else
+    {
+        if (fd == STDIN_FILENO)
+            add_event(epollfd,sockfd,EPOLLOUT);
+        else
+        {
+            delete_event(epollfd,sockfd,EPOLLIN);
+            add_event(epollfd,STDOUT_FILENO,EPOLLOUT);
+        }
+    }
+}
+
+static void do_write(int epollfd,int fd,int sockfd,char *buf)
+{
+    int nwrite;
+    nwrite = write(fd,buf,strlen(buf));
+    if (nwrite == -1)
+    {
+        perror("write error:");
+        close(fd);
+    }
+    else
+    {
+        if (fd == STDOUT_FILENO)
+            delete_event(epollfd,fd,EPOLLOUT);
+        else
+            modify_event(epollfd,fd,EPOLLIN);
+    }
+    memset(buf,0,MAXSIZE);
+}
+
+static void add_event(int epollfd,int fd,int state)
+{
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&ev);
+}
+
+static void delete_event(int epollfd,int fd,int state)
+{
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_DEL,fd,&ev);
+}
+
+static void modify_event(int epollfd,int fd,int state)
+{
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&ev);
 }
--- linux/example3/server3.cpp
+++ linux/example3/server3.cpp
@@ -2,226 +2,186 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/select.h>
-#include <sys/types.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/epoll.h>
 #include <unistd.h>
-#include <assert.h>
+#include <sys/types.h>
 
-#define IPADDR      "127.0.0.1"
+#define IPADDRESS   "127.0.0.1"
 #define PORT        8787
-#define MAXLINE     1024
+#define MAXSIZE     1024
 #define LISTENQ     5
-#define SIZE        10
+#define FDSIZE      1000
+#define EPOLLEVENTS 100
 
-typedef struct server_context_st
+//函数声明
+//创建套接字并进行绑定
+static int socket_bind(const char* ip,int port);
+//IO多路复用epoll
+static void do_epoll(int listenfd);
+//事件处理函数
+static void
+handle_events(int epollfd,struct epoll_event *events,int num,int listenfd,char *buf);
+//处理接收到的连接
+static void handle_accpet(int epollfd,int listenfd);
+//读处理
+static void do_read(int epollfd,int fd,char *buf);
+//写处理
+static void do_write(int epollfd,int fd,char *buf);
+//添加事件
+static void add_event(int epollfd,int fd,int state);
+//修改事件
+static void modify_event(int epollfd,int fd,int state);
+//删除事件
+static void delete_event(int epollfd,int fd,int state);
+
+int main(int argc,char *argv[])
 {
-    int cli_cnt;        /*客户端个数*/
-    int clifds[SIZE];   /*客户端的个数*/
-    fd_set allfds;      /*句柄集合*/
-    int maxfd;          /*句柄最大值*/
-} server_context_st;
-static server_context_st *s_srv_ctx = NULL;
-/*===========================================================================
- * ==========================================================================*/
-static int create_server_proc(const char* ip,int port)
+    int  listenfd;
+    listenfd = socket_bind(IPADDRESS,PORT);
+    listen(listenfd,LISTENQ);
+    do_epoll(listenfd);
+    return 0;
+}
+
+static int socket_bind(const char* ip,int port)
 {
-    int  fd;
+    int  listenfd;
     struct sockaddr_in servaddr;
-    fd = socket(AF_INET, SOCK_STREAM,0);
-    if (fd == -1) {
-        fprintf(stderr, "create socket fail,erron:%d,reason:%s\n",
-                errno, strerror(errno));
-        return -1;
+    listenfd = socket(AF_INET,SOCK_STREAM,0);
+    if (listenfd == -1)
+    {
+        perror("socket error:");
+        exit(1);
     }
-
-    /*一个端口释放后会等待两分钟之后才能再被使用，SO_REUSEADDR是让端口释放后立即就可以被再次使用。*/
-    int reuse = 1;
-    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1) {
-        return -1;
-    }
-
     bzero(&servaddr,sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     inet_pton(AF_INET,ip,&servaddr.sin_addr);
     servaddr.sin_port = htons(port);
-
-    if (bind(fd,(struct sockaddr*)&servaddr,sizeof(servaddr)) == -1) {
+    if (bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) == -1)
+    {
         perror("bind error: ");
-        return -1;
+        exit(1);
     }
-
-    listen(fd,LISTENQ);
-
-    return fd;
+    return listenfd;
 }
 
-static int accept_client_proc(int srvfd)
+static void do_epoll(int listenfd)
 {
-    struct sockaddr_in cliaddr;
-    socklen_t cliaddrlen;
-    cliaddrlen = sizeof(cliaddr);
-    int clifd = -1;
-
-    printf("accpet clint proc is called.\n");
-
-ACCEPT:
-    clifd = accept(srvfd,(struct sockaddr*)&cliaddr,&cliaddrlen);
-
-    if (clifd == -1) {
-        if (errno == EINTR) {
-            goto ACCEPT;
-        } else {
-            fprintf(stderr, "accept fail,error:%s\n", strerror(errno));
-            return -1;
-        }
+    int epollfd;
+    struct epoll_event events[EPOLLEVENTS];
+    int ret;
+    char buf[MAXSIZE];
+    memset(buf,0,MAXSIZE);
+    //创建一个描述符
+    epollfd = epoll_create(FDSIZE);
+    //添加监听描述符事件
+    add_event(epollfd,listenfd,EPOLLIN);
+    for ( ; ; )
+    {
+        //获取已经准备好的描述符事件
+        ret = epoll_wait(epollfd,events,EPOLLEVENTS,-1);
+        handle_events(epollfd,events,ret,listenfd,buf);
     }
-
-    fprintf(stdout, "accept a new client: %s:%d\n",
-            inet_ntoa(cliaddr.sin_addr),cliaddr.sin_port);
-
-    //将新的连接描述符添加到数组中
-    int i = 0;
-    for (i = 0; i < SIZE; i++) {
-        if (s_srv_ctx->clifds[i] < 0) {
-            s_srv_ctx->clifds[i] = clifd;
-            s_srv_ctx->cli_cnt++;
-            break;
-        }
-    }
-
-    if (i == SIZE) {
-        fprintf(stderr,"too many clients.\n");
-        return -1;
-    }
+    close(epollfd);
 }
 
-static int handle_client_msg(int fd, char *buf) 
+static void
+handle_events(int epollfd,struct epoll_event *events,int num,int listenfd,char *buf)
 {
-    assert(buf);
-    printf("recv buf is :%s\n", buf);
-    write(fd, buf, strlen(buf) +1);
-    return 0;
+    int i;
+    int fd;
+    //进行选好遍历
+    for (i = 0;i < num;i++)
+    {
+        fd = events[i].data.fd;
+        //根据描述符的类型和事件类型进行处理
+        if ((fd == listenfd) &&(events[i].events & EPOLLIN))
+            handle_accpet(epollfd,listenfd);
+        else if (events[i].events & EPOLLIN)
+            do_read(epollfd,fd,buf);
+        else if (events[i].events & EPOLLOUT)
+            do_write(epollfd,fd,buf);
+    }
 }
-
-static void recv_client_msg(fd_set *readfds)
+static void handle_accpet(int epollfd,int listenfd)
 {
-    int i = 0, n = 0;
     int clifd;
-    char buf[MAXLINE] = {0};
-    for (i = 0;i <= s_srv_ctx->cli_cnt;i++) {
-        clifd = s_srv_ctx->clifds[i];
-        if (clifd < 0) {
-            continue;
-        }
-        /*判断客户端套接字是否有数据*/
-        if (FD_ISSET(clifd, readfds)) {
-            //接收客户端发送的信息
-            n = read(clifd, buf, MAXLINE);
-            if (n <= 0) {
-                /*n==0表示读取完成，客户都关闭套接字*/
-                FD_CLR(clifd, &s_srv_ctx->allfds);
-                close(clifd);
-                s_srv_ctx->clifds[i] = -1;
-                continue;
-            }
-            handle_client_msg(clifd, buf);
-        }
-    }
-}
-static void handle_client_proc(int srvfd)
-{
-    int  clifd = -1;
-    int  retval = 0;
-    fd_set *readfds = &s_srv_ctx->allfds;
-    struct timeval tv;
-    int i = 0;
-
-    while (1) {
-        /*每次调用select前都要重新设置文件描述符和时间，因为事件发生后，文件描述符和时间都被内核修改啦*/
-        FD_ZERO(readfds);
-        /*添加监听套接字*/
-        FD_SET(srvfd, readfds);
-        s_srv_ctx->maxfd = srvfd;
-
-        tv.tv_sec = 30;
-        tv.tv_usec = 0;
-        /*添加客户端套接字*/
-        for (i = 0; i < s_srv_ctx->cli_cnt; i++) {
-            clifd = s_srv_ctx->clifds[i];
-            /*去除无效的客户端句柄*/
-            if (clifd != -1) {
-                FD_SET(clifd, readfds);
-            }
-            s_srv_ctx->maxfd = (clifd > s_srv_ctx->maxfd ? clifd : s_srv_ctx->maxfd);
-        }
-
-        /*开始轮询接收处理服务端和客户端套接字*/
-        retval = select(s_srv_ctx->maxfd + 1, readfds, NULL, NULL, &tv);
-        if (retval == -1) {
-            fprintf(stderr, "select error:%s.\n", strerror(errno));
-            return;
-        }
-        if (retval == 0) {
-            fprintf(stdout, "select is timeout.\n");
-            continue;
-        }
-        if (FD_ISSET(srvfd, readfds)) {
-            /*监听客户端请求*/
-            accept_client_proc(srvfd);
-        } else {
-            /*接受处理客户端消息*/
-            recv_client_msg(readfds);
-        }
+    struct sockaddr_in cliaddr;
+    socklen_t  cliaddrlen;
+    clifd = accept(listenfd,(struct sockaddr*)&cliaddr,&cliaddrlen);
+    if (clifd == -1)
+        perror("accpet error:");
+    else
+    {
+        printf("accept a new client: %s:%d\n",inet_ntoa(cliaddr.sin_addr),cliaddr.sin_port);
+        //添加一个客户描述符和事件
+        add_event(epollfd,clifd,EPOLLIN);
     }
 }
 
-static void server_uninit()
+static void do_read(int epollfd,int fd,char *buf)
 {
-    if (s_srv_ctx) {
-        free(s_srv_ctx);
-        s_srv_ctx = NULL;
+    int nread;
+    nread = read(fd,buf,MAXSIZE);
+    if (nread == -1)
+    {
+        perror("read error:");
+        close(fd);
+        delete_event(epollfd,fd,EPOLLIN);
+    }
+    else if (nread == 0)
+    {
+        fprintf(stderr,"client close.\n");
+        close(fd);
+        delete_event(epollfd,fd,EPOLLIN);
+    }
+    else
+    {
+        printf("read message is : %s",buf);
+        //修改描述符对应的事件，由读改为写
+        modify_event(epollfd,fd,EPOLLOUT);
     }
 }
 
-static int server_init()
+static void do_write(int epollfd,int fd,char *buf)
 {
-    s_srv_ctx = (server_context_st *)malloc(sizeof(server_context_st));
-    if (s_srv_ctx == NULL) {
-        return -1;
+    int nwrite;
+    nwrite = write(fd,buf,strlen(buf));
+    if (nwrite == -1)
+    {
+        perror("write error:");
+        close(fd);
+        delete_event(epollfd,fd,EPOLLOUT);
     }
-
-    memset(s_srv_ctx, 0, sizeof(server_context_st));
-
-    int i = 0;
-    for (;i < SIZE; i++) {
-        s_srv_ctx->clifds[i] = -1;
-    }
-
-    return 0;
+    else
+        modify_event(epollfd,fd,EPOLLIN);
+    memset(buf,0,MAXSIZE);
 }
 
-int main(int argc,char *argv[])
+static void add_event(int epollfd,int fd,int state)
 {
-    int  srvfd;
-    /*初始化服务端context*/
-    if (server_init() < 0) {
-        return -1;
-    }
-    /*创建服务,开始监听客户端请求*/
-    srvfd = create_server_proc(IPADDR, PORT);
-    if (srvfd < 0) {
-        fprintf(stderr, "socket create or bind fail.\n");
-        goto err;
-    }
-    /*开始接收并处理客户端请求*/
-    handle_client_proc(srvfd);
-    server_uninit();
-    return 0;
-err:
-    server_uninit();
-    return -1;
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&ev);
+}
+
+static void delete_event(int epollfd,int fd,int state)
+{
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_DEL,fd,&ev);
+}
+
+static void modify_event(int epollfd,int fd,int state)
+{
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&ev);
 }
