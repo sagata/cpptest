48bddabcc160443848180fc0f28321efd301a191
xyn3037 <xyn3037@corp.netease.com>
2018-03-06 15:06:53 +0800

    a

--- linux/example2/server2.cpp
+++ linux/example2/server2.cpp
@@ -8,19 +8,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/shm.h>
-#include <thread>
 #include <iostream>
 #define PORT 7000
 #define QUEUE 20
-int conn;
-void thread_task() {
-}
 
 int main() {
-    //printf("%d\n",AF_INET);
-    //printf("%d\n",SOCK_STREAM);
+    fd_set rfds;
+    struct timeval tv;
+    int retval, maxfd;
     int ss = socket(AF_INET, SOCK_STREAM, 0);
-    //printf("%d\n",ss);
     struct sockaddr_in server_sockaddr;
     server_sockaddr.sin_family = AF_INET;
     server_sockaddr.sin_port = htons(PORT);
@@ -38,31 +34,51 @@ int main() {
     struct sockaddr_in client_addr;
     socklen_t length = sizeof(client_addr);
     ///成功返回非负描述字，出错返回-1
-    conn = accept(ss, (struct sockaddr*)&client_addr, &length);
+    int conn = accept(ss, (struct sockaddr*)&client_addr, &length);
     if( conn < 0 ) {
         perror("connect");
         exit(1);
     }
-
-    char buffer[1024];
-    //创建另外一个线程
-    //std::thread t(thread_task);
-    //t.join();
-    //char buf[1024];
-    //主线程
     while(1) {
-
-        // memset(buf, 0 ,sizeof(buf));
-        // if(fgets(buf, sizeof(buf),stdin) != NULL) {
-        //     send(conn, buf, sizeof(buf), 0);    
-        // }
-
-        memset(buffer, 0 ,sizeof(buffer));
-        int len = recv(conn, buffer, sizeof(buffer), 0);
-        if(strcmp(buffer, "exit\n") == 0) break;
-        printf("%s", buffer);
-        //必须要有返回数据， 这样才算一个完整的请求
-        send(conn, buffer, len , 0);
+        /*把可读文件描述符的集合清空*/
+        FD_ZERO(&rfds);
+        /*把标准输入的文件描述符加入到集合中*/
+        FD_SET(0, &rfds);
+        maxfd = 0;
+        /*把当前连接的文件描述符加入到集合中*/
+        FD_SET(conn, &rfds);
+        /*找出文件描述符集合中最大的文件描述符*/    
+        if(maxfd < conn)
+            maxfd = conn;
+        /*设置超时时间*/
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        /*等待聊天*/
+        retval = select(maxfd+1, &rfds, NULL, NULL, &tv);
+        if(retval == -1){
+            printf("select出错，客户端程序退出\n");
+            break;
+        }else if(retval == 0){
+            printf("服务端没有任何输入信息，并且客户端也没有信息到来，waiting...\n");
+            continue;
+        }else{
+            /*客户端发来了消息*/
+            if(FD_ISSET(conn,&rfds)){
+                char buffer[1024];    
+                memset(buffer, 0 ,sizeof(buffer));
+                int len = recv(conn, buffer, sizeof(buffer), 0);
+                if(strcmp(buffer, "exit\n") == 0) break;
+                printf("%s", buffer);
+                //send(conn, buffer, len , 0);把数据回发给客户端
+            }
+            /*用户输入信息了,开始处理信息并发送*/
+            if(FD_ISSET(0, &rfds)){
+                char buf[1024];
+                fgets(buf, sizeof(buf), stdin);
+                //printf("you are send %s", buf);
+                send(conn, buf, sizeof(buf), 0);    
+            }
+        }
     }
     close(conn);
     close(ss);
