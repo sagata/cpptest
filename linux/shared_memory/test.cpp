a752578ac026d4ddfc40e2683a2e12f055bbae71
xyn3037 <xyn3037@debian-owncloud-opt>
2018-03-20 17:10:49 +0800

    de

--- linux/shared_memory/test.cpp
+++ linux/shared_memory/test.cpp
@@ -4,10 +4,31 @@
 #include <stdlib.h>
 #include <sys/shm.h>
 
-int main()
+void del_mem()
+{
+    shmctl(65536, IPC_RMID, 0);
+}
+int create_mem()
 {
 	int shmid = shmget((key_t)1234, 100, 0666|IPC_CREAT);
 	printf("shmid %d\n", shmid);
-	
-	return 0;
+    return shmid;
+}
+void * attach_mem(int shmid)
+{
+	void *shm = shmat(shmid, 0, 0);
+    return shm;
+}
+void detach_mem(void *shm)
+{
+	shmdt(shm);
+}
+int main()
+{
+    //del_mem();	
+	int id = create_mem();
+    void * shm = attach_mem(id);
+    detach_mem(shm);
+    getchar();
+    return 0;
 }
